b1456ede084411b681ca190d5f3f945a5dfa3a07
ivan-kondratev <vk15555555@gmail.com>
2025-06-11 20:40:28 +0300

    added ripple

--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -1,10 +1,11 @@
 import { Page } from "@/components";
 import { Link } from "react-router-dom";
-import {useQuery} from "@tanstack/react-query";
-import {supabase} from "@/lib/supabase/client.ts";
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@/lib/supabase/client.ts";
+import { Ripple } from "@/components/ui/Ripple/Ripple";
 
 export const Chats = () => {
-    const {data, isLoading} = useQuery({
+    const { data, isLoading } = useQuery({
         queryFn: async () => {
             // 1) Формируем запрос, вызываем .select(...).maybeSingle()/.then()/.throwOnError()
             if (!supabase) return []
@@ -22,7 +23,7 @@ export const Chats = () => {
         },
         queryKey: ['chats']
     })
-    if(isLoading){
+    if (isLoading) {
         return (
             <Page>
                 <div className="profile-loading">
@@ -38,22 +39,24 @@ export const Chats = () => {
                 <h2 className={'font-bold text-xl p-4 pt-8'}>Важные чаты</h2>
                 <div className={'relative overflow-hidden bg-[url("/bg3.jpg")] bg-cover bg-top p-4 rounded-t-3xl flex-1 flex flex-col gap-3'}>
                     <img src={'/cube.png'} alt={''}
-                         className={'absolute min-w-[670px] -top-[40px] -z-[0] -right-[350px]'}/>
+                        className={'absolute min-w-[670px] -top-[40px] -z-[0] -right-[350px]'} />
                     <div className={'flex flex-col gap-3 relative z-[2]'}>
                         {data?.map(el => (
-                            <Link className={'p-4 rounded-3xl bg-white flex items-center justify-between gap-3'}
-                                  to={el.link}>
-                                <div className={'flex items-center gap-3'}>
-                                    <div className={'min-w-[48px] h-[48px] bg-gray-200 rounded-full'}>
+                            <Ripple key={el.id} className="rounded-3xl overflow-hidden">
+                                <Link className={'p-4 rounded-3xl bg-white flex items-center justify-between gap-3 block'}
+                                    to={el.link}>
+                                    <div className={'flex items-center gap-3'}>
+                                        <div className={'min-w-[48px] h-[48px] bg-gray-200 rounded-full'}>
 
+                                        </div>
+                                        <div className={'flex flex-col gap-1'}>
+                                            <p className={' font-semibold'}>{el.title}</p>
+                                            <p className={'text-sm font-medium text-[#9F9F9F]'}>{el.description}</p>
+                                        </div>
                                     </div>
-                                    <div className={'flex flex-col gap-1'}>
-                                        <p className={' font-semibold'}>{el.title}</p>
-                                        <p className={'text-sm font-medium text-[#9F9F9F]'}>{el.description}</p>
-                                    </div>
-                                </div>
-                                <img src={'/arrow-icon.svg'} alt="" className={'w-[36px] h-[36px]'}/>
-                            </Link>
+                                    <img src={'/arrow-icon.svg'} alt="" className={'w-[36px] h-[36px]'} />
+                                </Link>
+                            </Ripple>
                         ))}
                     </div>
                 </div>
--- src/pages/HelpPage/HelpPage.tsx
+++ src/pages/HelpPage/HelpPage.tsx
@@ -1,17 +1,18 @@
-import {Page} from "@/components";
-import {Link} from "react-router-dom";
-import {supabase} from "@/lib/supabase/client.ts";
+import { Page } from "@/components";
+import { Link } from "react-router-dom";
+import { supabase } from "@/lib/supabase/client.ts";
 import {
     initDataState as _initDataState,
     useSignal,
 } from '@telegram-apps/sdk-react';
-import {useSupabaseUser} from "@/lib/supabase/hooks";
-import {useQuery} from "@tanstack/react-query";
+import { useSupabaseUser } from "@/lib/supabase/hooks";
+import { useQuery } from "@tanstack/react-query";
+import { Ripple } from "@/components/ui/Ripple/Ripple";
 
-export const HelpPage = ()=> {
+export const HelpPage = () => {
     const initDataState = useSignal(_initDataState);
-    const {supabaseUser} = useSupabaseUser(initDataState);
-    const {data: userCurator, isLoading: isLoadingCurator} = useQuery({
+    const { supabaseUser } = useSupabaseUser(initDataState);
+    const { data: userCurator, isLoading: isLoadingCurator } = useQuery({
         queryFn: async () => {
             if (!supabase || !supabaseUser?.id) return undefined
             const { data, error } = await supabase?.from('user_curator').select('*').eq('student_id', supabaseUser.id).single()
@@ -26,7 +27,7 @@ export const HelpPage = ()=> {
         queryKey: ['user_curator', supabaseUser?.id],
         enabled: !!supabaseUser?.id
     })
-    const {data, isLoading} = useQuery({
+    const { data, isLoading } = useQuery({
         queryFn: async () => {
             // 1) Формируем запрос, вызываем .select(...).maybeSingle()/.then()/.throwOnError()
             if (!supabase) return []
@@ -43,7 +44,7 @@ export const HelpPage = ()=> {
         },
         queryKey: ['curators']
     })
-    if(!supabaseUser?.id || !data || isLoadingCurator || isLoading){
+    if (!supabaseUser?.id || !data || isLoadingCurator || isLoading) {
         return (
             <Page>
                 <div className="profile-loading">
@@ -53,34 +54,40 @@ export const HelpPage = ()=> {
             </Page>
         )
     }
-    return(
+    return (
         <Page>
             <div className={'flex flex-col gap-4 text-black min-h-[calc(100vh-60px)]'}>
                 <h2 className={'font-bold text-xl p-4 pb-0 pt-12'}>Ваш куратор</h2>
-                {userCurator && <Link to={`https://t.me/${data?.filter(el => el.id === userCurator?.curator_id)?.[0].username}`} className={'px-4 flex items-center gap-4 justify-between'}>
-                    <div className={'flex items-center gap-3'}>
-                        <img src={data?.filter(el => el.id === userCurator?.curator_id)?.[0].photo_url} alt={''}
-                             className={'w-[60px] h-[60px] rounded-full'}/>
-                        <p className={'font-semibold text-xl'}>{data?.filter(el => el.id === userCurator?.curator_id)?.[0].first_name}</p>
-                    </div>
-                    <img src={'/arrow-icon.svg'} alt={''}/>
-                </Link>}
+                {userCurator && <div className="px-4">
+                    <Ripple className="rounded-2xl overflow-hidden inline-block w-full">
+                        <Link to={`https://t.me/${data?.filter(el => el.id === userCurator?.curator_id)?.[0].username}`} className={'flex items-center gap-4 justify-between block w-full'}>
+                            <div className={'flex items-center gap-3'}>
+                                <img src={data?.filter(el => el.id === userCurator?.curator_id)?.[0].photo_url} alt={''}
+                                    className={'w-[60px] h-[60px] rounded-full'} />
+                                <p className={'font-semibold text-xl'}>{data?.filter(el => el.id === userCurator?.curator_id)?.[0].first_name}</p>
+                            </div>
+                            <img src={'/arrow-icon.svg'} alt={''} />
+                        </Link>
+                    </Ripple>
+                </div>}
                 <div className={'bg-[url("/bg3.jpg")] relative overflow-hidden bg-cover bg-top p-4 rounded-t-3xl flex-1 flex flex-col gap-3'}>
                     <img src={'/crystall.png'} alt={''}
-                         className={'opacity-70 absolute min-w-[700px] -top-[100px] -z-[0] -right-[300px] -rotate-[10deg]'}/>
+                        className={'opacity-70 absolute min-w-[700px] -top-[100px] -z-[0] -right-[300px] -rotate-[10deg]'} />
                     <p className={'font-bold text-xl relative z-[2]'}>Другие кураторы</p>
                     <div className={'flex flex-col gap-3 relative z-[2]'}>
                         {data?.filter(el => el.id !== (userCurator?.curator_id || undefined)).map((el, i) => (
-                            <Link key={i} className={'p-4 rounded-3xl bg-white flex justify-between gap-2 items-center'}
-                                  to={`https://t.me/${el.username}`}>
-                                <div className={'flex items-center gap-2'}>
-                                    {el.photo_url ?
-                                        <img src={el.photo_url} className={'w-12 h-12 rounded-full object-cover'}
-                                             alt={''}/> : <div className={'w-12 h-12 rounded-full bg-[#EEEEEE]'}></div>}
-                                    <p className={'font-semibold'}>{el.first_name}</p>
-                                </div>
-                                <img src={'/arrow-icon.svg'} alt={''}/>
-                            </Link>
+                            <Ripple key={i} className="rounded-3xl overflow-hidden">
+                                <Link className={'p-4 rounded-3xl bg-white flex justify-between gap-2 items-center block'}
+                                    to={`https://t.me/${el.username}`}>
+                                    <div className={'flex items-center gap-2'}>
+                                        {el.photo_url ?
+                                            <img src={el.photo_url} className={'w-12 h-12 rounded-full object-cover'}
+                                                alt={''} /> : <div className={'w-12 h-12 rounded-full bg-[#EEEEEE]'}></div>}
+                                        <p className={'font-semibold'}>{el.first_name}</p>
+                                    </div>
+                                    <img src={'/arrow-icon.svg'} alt={''} />
+                                </Link>
+                            </Ripple>
                         ))}
                     </div>
                 </div>
