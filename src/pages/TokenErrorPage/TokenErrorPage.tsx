d937dc6c64e80b41569fc692ad7d9caf8ba41f44
ivan-kondratev <vk15555555@gmail.com>
2025-06-14 03:25:28 +0300

    Выровнена кнопка

--- src/pages/TokenErrorPage/TokenErrorPage.tsx
+++ src/pages/TokenErrorPage/TokenErrorPage.tsx
@@ -1,5 +1,5 @@
-import {Link, useLocation} from 'react-router-dom';
-import {Ripple} from "@/components/ui/Ripple/Ripple.tsx";
+import { Link, useLocation } from 'react-router-dom';
+import { Ripple } from "@/components/ui/Ripple/Ripple.tsx";
 
 const TokenErrorPage = () => {
     const location = useLocation();
@@ -9,24 +9,26 @@ const TokenErrorPage = () => {
     const isNoAccessError = !error;
 
     const title = isNoAccessError ?
-        <span>Приложение<br/> доступно только<br/> для учеников программы Brain Programming</span> : 'Ошибка активации доступа';
+        <span>Приложение<br /> доступно только<br /> для учеников программы Brain Programming</span> : 'Ошибка активации доступа';
 
 
 
     return (
         <div className="flex flex-col py-6 justify-between gap-6 items-center min-h-screen bg-[linear-gradient(180deg,_#D5D9F4_0%,_#E3E0F7_33.65%,_#EDF7FE_68.27%,_#E8F1FD_100%)]">
             <div className={'flex flex-col items-center gap-5'}>
-                <img src={'/hand.png'} alt={''} className={'w-[250px]'}/>
+                <img src={'/hand.png'} alt={''} className={'w-[250px]'} />
                 <h1 className={'font-bold text-2xl px-4 leading-6 text-center text-black'}>{title}</h1>
             </div>
-            <Ripple className="rounded-3xl overflow-hidden inline-block">
-                <a className={'w-full px-4 max-w-[350px]'} href={'https://t.me/katyaasta'} target={'_blank'}>
-                    <button
-                        className={'w-full font-bold p-4 rounded-full bg-[linear-gradient(135deg,rgba(141,197,241,0.4)_-48.61%,#63ABE6_105.56%),linear-gradient(91.99deg,#F3F3F3_0%,#EAEAEA_100%)]'}>
-                        Обратиться в отдел заботы
-                    </button>
-                </a>
-            </Ripple>
+            <div className="w-full max-w-xs px-4">
+                <Ripple className="rounded-3xl overflow-hidden">
+                    <a className={'block w-full'} href={'https://t.me/katyaasta'} target={'_blank'}>
+                        <button
+                            className={'w-full font-bold p-4 rounded-full bg-[linear-gradient(135deg,rgba(141,197,241,0.4)_-48.61%,#63ABE6_105.56%),linear-gradient(91.99deg,#F3F3F3_0%,#EAEAEA_100%)]'}>
+                            Обратиться в отдел заботы
+                        </button>
+                    </a>
+                </Ripple>
+            </div>
 
             {/*<div className="error-card">
                 <h1>{title}</h1>
