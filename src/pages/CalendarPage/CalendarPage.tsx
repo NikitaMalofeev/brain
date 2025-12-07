a81aa7ce44e8d973d1c7e0a4c5ee566607e50ce5
Nikita Malofeev <front.work3@gmail.com>
2025-12-07 20:07:03 +0300

    +

--- src/pages/CalendarPage/CalendarPage.tsx
+++ src/pages/CalendarPage/CalendarPage.tsx
@@ -231,7 +231,7 @@ const CalendarPage: React.FC = () => {
   return (
     <Page back={false}>
       <div
-        className="calendar-page with-content-offset"
+        className="calendar-page with-content-offset-small"
         style={{
           backgroundImage: `url(${Background1})`,
           backgroundSize: 'cover',
